313a1595badc5ca91e241ec61e42689cfb1bd614
mio256 <reiya@woorth.io>
2024-05-17 23:51:59 +0900

    :zap: logging; slog

--- main.go
+++ main.go
@@ -1,12 +1,50 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/mio256/thirdbook/ui/api"
 	"github.com/mio256/thirdbook/usecase/handler"
+	"github.com/ogen-go/ogen/middleware"
 )
+
+func main() {
+	var slogOpts slog.HandlerOptions
+	slogOpts.Level = slog.LevelDebug
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slogOpts))
+	slog.SetDefault(logger)
+
+	ctx := context.Background()
+
+	if err := run(ctx); err != nil {
+		slog.ErrorContext(ctx, err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	h := handler.NewHandler()
+	s := handler.NewSecurityHandler()
+	srv, err := api.NewServer(h, s, api.WithMiddleware(loggingMiddleware()))
+	if err != nil {
+		return err
+	}
+
+	// Wrap the server with the CORS middleware
+	corsHandler := enableCORS(srv)
+
+	addr := ":8080"
+	slog.InfoContext(ctx, "Starting HTTP Server", slog.String("addr", addr))
+	if err := http.ListenAndServe(addr, corsHandler); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -27,18 +65,27 @@ func enableCORS(next http.Handler) http.Handler {
 	})
 }
 
-func main() {
-	h := handler.NewHandler()
-	s := handler.NewSecurityHandler()
-	srv, err := api.NewServer(h, s)
-	if err != nil {
-		log.Fatal(err)
-	}
+func loggingMiddleware() middleware.Middleware {
+	return func(req middleware.Request, next middleware.Next) (middleware.Response, error) {
+		slog.DebugContext(
+			req.Context, fmt.Sprintf("[http] <-- %s", req.OperationName),
+			slog.String("path", req.Raw.URL.String()),
+			slog.Any("body", req.Body),
+		)
 
-	// Wrap the server with the CORS middleware
-	corsHandler := enableCORS(srv)
+		resp, err := next(req)
+		if err != nil {
+			slog.DebugContext(
+				req.Context, fmt.Sprintf("FAIL %s", req.OperationName),
+				slog.String("message", err.Error()),
+			)
+		} else {
+			slog.DebugContext(
+				req.Context, fmt.Sprintf("[http] --> %s", req.OperationName),
+				slog.Any("body", resp.Type),
+			)
+		}
 
-	if err := http.ListenAndServe(":8080", corsHandler); err != nil {
-		log.Fatal(err)
+		return resp, err
 	}
 }
