8305ee52227198f3a178c5f0c01b5ed51e108ceb
mio256 <reiya@woorth.io>
2024-05-18 00:10:14 +0900

    :bug: typo infra package

--- pkg/infra/db_test.go
+++ pkg/infra/db_test.go
@@ -1,15 +1,16 @@
-package infra
+package infra_test
 
 import (
 	"context"
 	"testing"
 
+	"github.com/mio256/thirdbook/pkg/infra"
 	"github.com/mio256/thirdbook/pkg/infra/rdb"
 )
 
 func Test_ConnectDB(t *testing.T) {
 	ctx := context.Background()
-	dbConn := ConnectDB(ctx)
+	dbConn := infra.ConnectDB(ctx)
 	repo := rdb.New(dbConn)
 	if err := repo.Ping(ctx); err != nil {
 		panic(err)
